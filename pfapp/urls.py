deea308bc3daf384e2022f8309c9ef1e257e561f
AltoTenor <theonetrueking2002@gmail.com>
2023-06-19 15:41:34 +0530

    login

--- pfapp/urls.py
+++ pfapp/urls.py
@@ -7,6 +7,8 @@ from django.contrib.auth import views as auth_views
 app_name="pfapp"
 urlpatterns = [
     path("",views.Get_started.as_view(),name="Get_started"),
-    path("",views.Get_started.as_view(),name=""),
+    # path("register/",views.Reigster.as_view(),name=""),
     path("accounts/login/", auth_views.LoginView.as_view()),
+    # path("dashboard/",views.Dashboard.as_view(),name="dashboard"),
+    # path("portfolio/<first-name>",views.Portfolio.as_view(),name="portfolio"),
 ]
