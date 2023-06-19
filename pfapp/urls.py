e90ed9dd298531b9f23d01193ff7152ae43efbce
AltoTenor <theonetrueking2002@gmail.com>
2023-06-20 01:28:56 +0530

    Basic flow made

--- pfapp/urls.py
+++ pfapp/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path("",views.Get_started.as_view(),name="Get_started"),
     path("register/",views.Register.as_view(),name="register"),
     path("accounts/login/", auth_views.LoginView.as_view()),
-    # path("dashboard/",views.Dashboard.as_view(),name="dashboard"),
+    path("dashboard/",views.Dashboard.as_view(),name="dashboard"),
+    path("portfolio/<slug:username>/",views.Portfolio.as_view(),name="portfolio"),
+    path("logout/",views.logout_view,name="logout"),
+
     # path("portfolio/<first-name>",views.Portfolio.as_view(),name="portfolio"),
 ]
--- pfapp/views.py
+++ pfapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect
 from django.views import View
 from .forms import NewUserForm
-from django.contrib.auth import login
+from django.contrib.auth import login,logout
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 class Get_started(View):                                 
@@ -20,6 +23,23 @@ class Register(View):
             user = form.save()
             login(request, user)
             messages.success(request, "Registration successful." )
-            return redirect("pfapp:Get_started")
+            return redirect("pfapp:dashboard")
         messages.error(request, "Unsuccessful registration. Invalid information.")
+        # print("Error")
         return redirect(request.path)
+    
+
+
+class Dashboard(LoginRequiredMixin,View):                                 
+    def get(self, request) :
+        return render(request,'pfapp/dashboard.html')
+    
+
+def logout_view(request):
+    logout(request)
+    messages.success(request,"Logged out successfully. ")
+    return redirect(reverse_lazy('pfapp:Get_started'))
+
+class Portfolio(View):                                 
+    def get(self, request , username) :
+        return render(request,'pfapp/portfolio.html',{'usr':username})
