83a8a71664c9dcc8781abb8eef558082b06a4d04
AltoTenor <theonetrueking2002@gmail.com>
2023-07-11 01:43:54 +0530

    api3

--- pfapp/urls.py
+++ pfapp/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("accounts/login/", views.UpdatedLoginView.as_view(extra_context={"leftclass":"d-none d-md-block"})),
     path("dashboard/",views.Dashboard.as_view(),name="dashboard"),
     # path("portfolio/<slug:num>/",views.Portfolio.as_view(),name="portfolio"),
-    path("portfolio/<slug:num>/",views.PortfolioAPI.as_view(),name="portfolio"),
+    path("portfolio/<slug:num>/<slug:userid>/",views.PortfolioAPI.as_view(),name="portfolio"),
     path("logout/",views.logout_view,name="logout"),
 
     # path("portfolio/<first-name>",views.Portfolio.as_view(),name="portfolio"),
