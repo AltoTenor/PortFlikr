f5c6023f6535819a0032dbe1dec6b9b09517eb42
AltoTenor <theonetrueking2002@gmail.com>
2023-07-10 13:04:57 +0530

    Mobile meh support

--- portflikr_proj/settings.py
+++ portflikr_proj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4=(-#y#5qf!c=uhnghu@m*=_gqfa$cg_(!ps+gp7yfz-p7damu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['altotenor2002.pythonanywhere.com']
+ALLOWED_HOSTS = ['altotenor2002.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
